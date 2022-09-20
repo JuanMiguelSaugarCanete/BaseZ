f94ada4b2a576d227f8697d2c268f2c4daa249b2
Juan MIguel Saugar Cañete <juanmiguelsaugarcanete@gmail.com>
2022-09-20 19:47:14 +0200

    Agregar archivos de proyecto.

---
+++ BaseZ/Models/Database.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaseZ.Models
+{
+    internal class Database
+    {
+        private string _nameDatabase;
+        private string _passwordDatabase;
+        private List<Register> _registers;
+    }
+}
---
+++ BaseZ/Models/Register.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaseZ.Models
+{
+    internal class Register
+    {
+        private string _id;
+        private string _name;
+        private string _email;
+        private string _userName;
+        private string _password;
+        private string _url;
+
+        public string Id { get => _id; set => _id = value; }
+        public string Name { get => _name; set => _name = value; }
+        public string Email { get => _email; set => _email = value; }
+        public string UserName { get => _userName; set => _userName = value; }
+        public string Password { get => _password; set => _password = value; }
+        public string Url { get => _url; set => _url = value; }
+    }
+}
