cc5da94748d20f5a8df3d3be519fc18c1b6be49b
Juan MIguel Saugar Cañete <juanmiguelsaugarcanete@gmail.com>
2022-09-20 20:53:38 +0200

    Crear ficheros json

--- BaseZ/Models/Database.cs
+++ BaseZ/Models/Database.cs
@@ -11,5 +11,22 @@ namespace BaseZ.Models
         private string _nameDatabase;
         private string _passwordDatabase;
         private List<Register> _registers;
+
+        public Database()
+        {
+            this._registers = new List<Register>();
+        }
+        public Database(string nameDatabase, string passwordDatabase)
+        {
+            this._nameDatabase = nameDatabase;
+            this._passwordDatabase = passwordDatabase;
+            this._registers = new List<Register>();
+        }
+
+        public Database createOneDatabase(string nameDatabase, string password) { 
+            Database database = new Database(nameDatabase,password);
+            database._registers.Add(new Register("1", "data", "data@data.es", "dataData", "123456", "www.google.es"));
+            return database;
+        }
     }
 }
--- BaseZ/Models/Register.cs
+++ BaseZ/Models/Register.cs
@@ -15,6 +15,20 @@ namespace BaseZ.Models
         private string _password;
         private string _url;
 
+        public Register()
+        {
+            
+        }
+        public Register(string id, string name, string email, string userName, string password, string url)
+        {
+            _id = id;
+            _name = name;
+            _email = email;
+            _userName = userName;
+            _password = password;
+            _url = url;
+        }
+
         public string Id { get => _id; set => _id = value; }
         public string Name { get => _name; set => _name = value; }
         public string Email { get => _email; set => _email = value; }
